5bf784dbf15201c839df541a9077733723f7d719
Drew <pikamarisa64@gmail.com>
2018-08-23 13:11:54 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class Main {
 	public static void main(String[] arguments) throws Exception
 	{
-		JDA api = new JDABuilder(AccountType.BOT).setToken("NDgxMTgxNDE5MDkyMDQ5OTY3.Dl4ZfA._OEINim336-oxJO5YXSngGcaqBc").buildAsync();
+		JDA api = new JDABuilder(AccountType.BOT).setToken("Fuck").buildAsync();
 		
 		api.addEventListener(new MyListener());
 	
